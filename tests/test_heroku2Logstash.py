7f4432b47d702e96bb9c8a53e5702dcdf8332392
Stephane Perrin <stephane.perrin@chauffeur-prive.com>
2017-06-27 14:06:17 +0200

    Add unittests for split errors

--- tests/test_heroku2Logstash.py
+++ tests/test_heroku2Logstash.py
@@ -43,6 +43,16 @@ class TestH2LApp(AsyncHTTPTestCase):
         self.assertEqual(h2l.stats["error"], 2)
         self.assertEqual(h2l.stats["timeout"], 0)
 
+    def test_H2L_split_error(self):
+        payload = b"50 <40>1 2017-06-14T13:52:29+00:00 host app web.3 - State changed from starting to up\n119 <40>1 2017-06-14T13:53:26+00:00 host app web.3 - Starting process with command `bundle exec rackup config.ru -p 24405`"
+        response = self.fetch('/heroku/v1/toto', method='POST', body=payload)
+        self.assertEqual(response.code, 200)
+        self.assertEqual(len(response.body), 0)
+        self.assertEqual(h2l.stats["input"], 1)
+        self.assertEqual(h2l.stats["output"], 0)
+        self.assertEqual(h2l.stats["error"], 1)
+        self.assertEqual(h2l.stats["timeout"], 0)
+
     def test_H2L_logstash_reply_500(self):
         request = tornado.httpclient.HTTPRequest('http://127.0.0.1:8888/heroku/v1/toto', method="POST")
         http_response = tornado.httpclient.HTTPResponse(request, 500, buffer='')
